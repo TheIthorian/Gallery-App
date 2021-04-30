9667cc8c845299dd98a8fd49657af806f16edcb9
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2021-04-30 19:52:59 +0100

    Removing config.py

--- src/config.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flaskext.mysql import MySQL
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'PAx_!@ZRlb*blgwc9vu?7w%P0&Fqq=Y_'
-app.config['MYSQL_DATABASE_DB'] = 'gallery'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
