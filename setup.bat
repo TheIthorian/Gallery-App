5873ba9914884c1bbc8ccc8f00541f0d7ab4e46c
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-05-14 11:33:09 +0100

    Fix setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d %~dp0\react & npm install & cd /d %~dp0\src & py virtualenv ./venv & cd /d %~dp0\src\venv\Scripts & activate & pip install requirements.txt & exit"
+cmd /k "cd /d %~dp0\react & npm install & cd /d %~dp0\src & py -m venv ./venv & cd /d %~dp0\src\venv\Scripts & activate & pip install -r ../../requirements.txt & pip install flask-mysql"
