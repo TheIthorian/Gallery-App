15a075c7abb815b90f20301ddcd6e06ac119bc92
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-07 17:33:03 +0100

    Fix formatting

--- react/src/components/popup.js
+++ react/src/components/popup.js
@@ -1,35 +1,36 @@
-import React from "react";
+import React from 'react';
 
 class Popup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isPopupOpenState : this.props.isPopupOpen
-        }
+            isPopupOpenState: this.props.isPopupOpen,
+        };
     }
-
 
     togglePopup = () => {
-        this.setState({isPopupOpenState: !this.state.isPopupOpenState});
+        this.setState({ isPopupOpenState: !this.state.isPopupOpenState });
         //this.props.isPopupOpen = this.state.isPopupOpenState;
-    }
+    };
 
     render() {
-        let {content, isPopupOpen, handleClose} = this.props;
-        let {isPopupOpenState} = this.state;
+        let { content, isPopupOpen, handleClose } = this.props;
+        let { isPopupOpenState } = this.state;
         if (isPopupOpen) {
             return (
-                <div key={this.props.isPopupOpen} className="popup-box">
-                    <div className="box">
-                        <button className="close-icon custom" onClick={handleClose}>x</button>
+                <div key={this.props.isPopupOpen} className='popup-box'>
+                    <div className='box'>
+                        <button className='close-icon custom' onClick={handleClose}>
+                            x
+                        </button>
                         {content}
                     </div>
                 </div>
             );
         } else {
-            return (<></>);
+            return <></>;
         }
     }
-};
+}
 
 export default Popup;
