6a574ce603a3b5d271b91ddafc61b66693a6cb8f
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-07 17:33:09 +0100

    Fix formatting

--- react/src/components/logout.js
+++ react/src/components/logout.js
@@ -1,38 +1,34 @@
-
-// Logs user out of current session and changes location 
+// Logs user out of current session and changes location
 // ** Unused **
 function Logout() {
-    let sessionId = localStorage.getItem("sessionId");
+    let sessionId = localStorage.getItem('sessionId');
 
     let requestOptions = {
         method: 'POST',
         headers: {
-            'sessionId': sessionId,
-            },
-        body: {"Data":"Data"}
-    }
+            sessionId: sessionId,
+        },
+        body: { Data: 'Data' },
+    };
 
-    console.log(requestOptions.sessionId);    
+    console.log(requestOptions.sessionId);
 
-    fetch("http://127.0.0.1:5000/Logout", requestOptions)
-    .then(res => res.json())
-    .then((data) => {
-      console.log(data);
-        if (data.Result == "Success") {
-          localStorage.setItem('isLoggedIn', false);
-          requestOptions.localStorage.setItem("sessionId", "");
-          alert('Logout Successful')
-          window.location.href = '/';
-      } else {
-          alert('Logout Failed: ' + data.Result);
-      }
-    })
-    .catch(console.log)        
+    fetch('http://127.0.0.1:5000/Logout', requestOptions)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.Result == 'Success') {
+                localStorage.setItem('isLoggedIn', false);
+                requestOptions.localStorage.setItem('sessionId', '');
+                alert('Logout Successful');
+                window.location.href = '/';
+            } else {
+                alert('Logout Failed: ' + data.Result);
+            }
+        })
+        .catch(console.log);
 
-    return(
-        <>
-        </>
-    );
-    }
+    return <></>;
+}
 
 export default Logout;
