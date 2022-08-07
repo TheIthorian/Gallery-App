8f32b893b28d52d2c26d60841ba2a6ed4d48ee8e
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-07 17:36:54 +0100

    Fix formatting

--- react/src/index.js
+++ react/src/index.js
@@ -1,16 +1,15 @@
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './app.js'
+import App from './app.js';
 
-import "./style/root.css";
-import "./style/buttons.css";
-import "./style/inputs.css";
-import "./style/scroll.css";
+import './style/root.css';
+import './style/buttons.css';
+import './style/inputs.css';
+import './style/scroll.css';
 
 ReactDOM.render(
-    //<Page />,
     <BrowserRouter>
         <App />
     </BrowserRouter>,
