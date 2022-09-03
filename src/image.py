e4d0f7027d82d961d03738f150a3d97497a0edbf
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-03 15:41:03 +0100

    Auto format

--- src/image.py
+++ src/image.py
@@ -2,9 +2,10 @@ import uuid
 from file_handler import get_image_from_file, remove_image, replace_image, save_image_to_file
 import server
 
-from user import UserProfile # remove
+from user import UserProfile  # remove
 
-def addImage (inputs, userProfile: UserProfile):
+
+def addImage(inputs, userProfile: UserProfile):
 
     responsePayload = server.ResponsePayload()
 
@@ -17,7 +18,7 @@ def addImage (inputs, userProfile: UserProfile):
             'Description': 'Data Authorisation Error: UserGalleryId'
         }
 
-        return [2, None, 'Data Authorisation Error: UserGalleryId'] # remove
+        return [2, None, 'Data Authorisation Error: UserGalleryId']  # remove
 
     if inputs['Image'] is None:
         inputs['Image'] = ""
@@ -26,61 +27,64 @@ def addImage (inputs, userProfile: UserProfile):
         inputs['URL'] = ""
 
     if len(inputs['URL']) == 0:
-        return [2, 
-        None, 
-        "Empty Image"]
+        return [2,
+                None,
+                "Empty Image"]
 
     title = inputs['Title']
-    url  = inputs['URL']
+    url = inputs['URL']
     garllery_id = inputs['GalleryId']
 
     filename = f"{uuid.uuid4().hex}"
 
     image_data = save_image_to_file(url, filename, userProfile)
 
-    # Insert the image data           
+    # Insert the image data
     queryInputs = {
-        'Title': title, 
-        'URL':url, 
-        'Path':filename,
-        'Suffix':image_data.mode,
-        'Width':image_data.size[0],
-        'Height':image_data.size[1],
-        'Status':1, 
-        'GalleryId':garllery_id,
-        'UserId':userProfile.userId,
-        'AddedByUserId':userProfile.userId
+        'Title': title,
+        'URL': url,
+        'Path': filename,
+        'Suffix': image_data.mode,
+        'Width': image_data.size[0],
+        'Height': image_data.size[1],
+        'Status': 1,
+        'GalleryId': garllery_id,
+        'UserId': userProfile.userId,
+        'AddedByUserId': userProfile.userId
     }
-    imageId = server.serverConnection.runInsertQuery("Image","ImageInsert", queryInputs)
+    imageId = server.serverConnection.runInsertQuery(
+        "Image", "ImageInsert", queryInputs)
 
-    image = server.serverConnection.runQuery("Image","GetImage", {'ImageId':imageId})[0]
+    image = server.serverConnection.runQuery(
+        "Image", "GetImage", {'ImageId': imageId})[0]
 
-    responsePayload.messages.append( {  
-        'GalleryId' : garllery_id,
+    responsePayload.messages.append({
+        'GalleryId': garllery_id,
         'ImageId': image['ImageId'],
-        'Title': image['Title'], 
+        'Title': image['Title'],
         'URL': image['URL'],
         'Image': '0'
     })
 
     try:
-        image_data = get_image_from_file(image['Path'], userProfile, image['Suffix'], image['Width'], image['Height'])
+        image_data = get_image_from_file(
+            image['Path'], userProfile, image['Suffix'], image['Width'], image['Height'])
     except:
         image_data = None
 
     return [
-        0, 
-        {   
-            'GalleryId' : inputs['GalleryId'],
+        0,
+        {
+            'GalleryId': inputs['GalleryId'],
             'ImageId': image['ImageId'],
-            'Title': image['Title'], 
+            'Title': image['Title'],
             'URL': image['URL'],
             'Image':image_data
-        }, 
+        },
         None]
 
 
-def updateImage (inputs, userProfile):
+def updateImage(inputs, userProfile):
 
     imageId = inputs['ImageId']
 
@@ -92,45 +96,48 @@ def updateImage (inputs, userProfile):
         return [2, None, 'Data Authorisation Error: GalleryIdImageId']
 
     if len(inputs['URL']) == 0:
-        return [2, 
-        None, 
-        "Empty Image"]
+        return [2,
+                None,
+                "Empty Image"]
 
-    image = server.serverConnection.runQuery("Image","GetImage", {'ImageId':imageId})[0]
+    image = server.serverConnection.runQuery(
+        "Image", "GetImage", {'ImageId': imageId})[0]
 
     image_data = replace_image(image['URL'], image['Path'], userProfile)
 
-    # Update the image data           
+    # Update the image data
     queryInputs = {
-        'ImageId':imageId, 
-        'Title':inputs['Title'], 
-        'URL':inputs['URL'],
-        'Suffix':image_data.mode,
-        'Width':image_data.size[0],
-        'Height':image_data.size[1],
-        'UserId':userProfile.userId
+        'ImageId': imageId,
+        'Title': inputs['Title'],
+        'URL': inputs['URL'],
+        'Suffix': image_data.mode,
+        'Width': image_data.size[0],
+        'Height': image_data.size[1],
+        'UserId': userProfile.userId
     }
-    server.serverConnection.runQuery("Image","ImageUpdate", queryInputs)
+    server.serverConnection.runQuery("Image", "ImageUpdate", queryInputs)
 
     return [
-        0, 
-        {   
-            'GalleryId' : inputs['GalleryId'],
+        0,
+        {
+            'GalleryId': inputs['GalleryId'],
             'ImageId': image['ImageId'],
-            'Title': image['Title'], 
+            'Title': image['Title'],
             'URL': image['URL'],
             'Image': get_image_from_file(image['Path'], userProfile, image_data.mode, image_data.size[0], image_data.size[1])
-        }, 
+        },
         None]
 
 
-def getImages (galleryId, userProfile):
+def getImages(galleryId, userProfile):
 
     if galleryId < 0:
-        images = server.serverConnection.runQuery("Image","GetImages", {'UserId':userProfile.userId})
-    
+        images = server.serverConnection.runQuery(
+            "Image", "GetImages", {'UserId': userProfile.userId})
+
     else:
-        images = server.serverConnection.runQuery("Image","GetGalleryImages", {'UserId':userProfile.userId, 'GalleryId': galleryId})
+        images = server.serverConnection.runQuery("Image", "GetGalleryImages", {
+                                                  'UserId': userProfile.userId, 'GalleryId': galleryId})
 
     output = []
 
@@ -138,64 +145,64 @@ def getImages (galleryId, userProfile):
 
         try:
             print(f"get_image_from_file: [{image['ImageId']}] {image['Path']}")
-            image_data = get_image_from_file(image['Path'], userProfile, image['Suffix'], image['Width'], image['Height'])
+            image_data = get_image_from_file(
+                image['Path'], userProfile, image['Suffix'], image['Width'], image['Height'])
         except:
             image_data = None
 
         output.append({
-            'GalleryId' : galleryId,
+            'GalleryId': galleryId,
             'ImageId': image['ImageId'],
-            'Title': image['Title'], 
+            'Title': image['Title'],
             'URL': image['URL'],
             'Image': image_data
         })
 
     return [
-        0, 
-        output, 
+        0,
+        output,
         None]
 
 
-def removeImage (inputs, userProfile):
+def removeImage(inputs, userProfile):
 
     galleryId = inputs['GalleryId']
     imageId = inputs['ImageId']
-        
+
     if not server.serverConnection.dataAuthorisation('UserGalleryId', userProfile.userId, galleryId):
         return [2, None, 'Data Authorisation Error: UserGalleryId']
 
     if not server.serverConnection.dataAuthorisation('GalleryIdImageId', galleryId, imageId):
         return [2, None, 'Data Authorisation Error: GalleryIdImageId']
 
-
-    image = server.serverConnection.runQuery("Image","GetImage", {'ImageId':imageId})[0]
+    image = server.serverConnection.runQuery(
+        "Image", "GetImage", {'ImageId': imageId})[0]
 
     remove_image(image['Path'])
 
     queryInputs = {
-            'UserId':userProfile.userId, 
-            'GalleryId':galleryId,
-            'ImageId':imageId
-        }
+        'UserId': userProfile.userId,
+        'GalleryId': galleryId,
+        'ImageId': imageId
+    }
 
     server.serverConnection.runQuery("Image", "DeleteImage", queryInputs)
 
     output = getImages(galleryId, userProfile)[1]
 
     return [
-        0, 
-        output, 
+        0,
+        output,
         None
-    ]       
+    ]
 
 
+def addGallery(inputs, userProfile):
 
-def addGallery (inputs, userProfile):
-
-    # Insert the gallery data           
+    # Insert the gallery data
     queryInputs = {
-        'Title':inputs['Title'], 
-        'UserId':userProfile.userId
+        'Title': inputs['Title'],
+        'UserId': userProfile.userId
     }
 
     server.serverConnection.runQuery("Image", "GalleryInsert", queryInputs)
@@ -203,7 +210,7 @@ def addGallery (inputs, userProfile):
     return getGallery({'GalleryId': -1}, userProfile)
 
 
-def getGallery (inputs, userProfile):
+def getGallery(inputs, userProfile):
 
     galleryId = inputs['GalleryId']
 
@@ -211,14 +218,15 @@ def getGallery (inputs, userProfile):
         return [2, None, 'Data Authorisation Error: UserGalleryId']
 
     if galleryId < 0:
-        gallerys = server.serverConnection.runQuery("Image", "GetAllGallery", {'UserId':userProfile.userId})
-    
+        gallerys = server.serverConnection.runQuery(
+            "Image", "GetAllGallery", {'UserId': userProfile.userId})
+
     else:
-        gallerys = server.serverConnection.runQuery("Image", "GetGallery", 
-            {
-                'UserId':userProfile.userId, 
-                'GalleryId':galleryId
-            })
+        gallerys = server.serverConnection.runQuery("Image", "GetGallery",
+                                                    {
+                                                        'UserId': userProfile.userId,
+                                                        'GalleryId': galleryId
+                                                    })
 
     output = []
 
@@ -226,71 +234,70 @@ def getGallery (inputs, userProfile):
         output.append({
             'GalleryId': gallery['GalleryId'],
             'Title': gallery['Title'],
-            'ImageCount':gallery['ImageCount']
+            'ImageCount': gallery['ImageCount']
         })
 
     return [
-        0, 
-        output, 
-        None]     
+        0,
+        output,
+        None]
 
 
-def updateGallery (inputs, userProfile):
+def updateGallery(inputs, userProfile):
 
     for data in inputs:
 
         galleryId = data['GalleryId']
-    
+
         if not server.serverConnection.dataAuthorisation('UserGalleryId', userProfile.userId, galleryId):
             return [2, None, 'Data Authorisation Error: UserGalleryId']
 
-
     for data in inputs:
 
-        title = data['Title'] 
+        title = data['Title']
         galleryId = data['GalleryId']
 
-        server.serverConnection.runQuery("Image", "CustomUpdateGallery", 
-            {
-                'UserId':userProfile.userId, 
-                'GalleryId':galleryId,
-                'Title':title
-            })
+        server.serverConnection.runQuery("Image", "CustomUpdateGallery",
+                                         {
+                                             'UserId': userProfile.userId,
+                                             'GalleryId': galleryId,
+                                             'Title': title
+                                         })
 
-        output = getGallery({'GalleryId':-1}, userProfile)[1]
+        output = getGallery({'GalleryId': -1}, userProfile)[1]
 
     return [
-        0, 
-        output, 
-        None]           
+        0,
+        output,
+        None]
 
 
-def removeGallery (inputs, userProfile):
+def removeGallery(inputs, userProfile):
 
     for data in inputs:
 
         galleryId = data['GalleryId']
-        
+
         if not server.serverConnection.dataAuthorisation('UserGalleryId', userProfile.userId, galleryId):
             return [2, None, 'Data Authorisation Error: UserGalleryId']
-
 
     for data in inputs:
 
         galleryId = data['GalleryId']
 
         inputs = {
-                'UserId':userProfile.userId, 
-                'GalleryId':galleryId
-            }
+            'UserId': userProfile.userId,
+            'GalleryId': galleryId
+        }
 
-        server.serverConnection.runQuery("Image", "DeleteGalleryImages", inputs)
+        server.serverConnection.runQuery(
+            "Image", "DeleteGalleryImages", inputs)
         server.serverConnection.runQuery("Image", "DeleteGallery", inputs)
 
-        output = getGallery({'GalleryId':-1}, userProfile)[1]
+        output = getGallery({'GalleryId': -1}, userProfile)[1]
         print(output)
 
     return [
-        0, 
-        output, 
-        None]       
+        0,
+        output,
+        None]
