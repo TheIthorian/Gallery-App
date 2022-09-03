cda76aef5afab6362d299db76505d90b653f183a
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-03 15:31:53 +0100

    Auto format

--- src/encryption.py
+++ src/encryption.py
@@ -2,21 +2,21 @@ import base64
 import cryptography
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from itertools import cycle
+
 
 def encrypt_xor(file_data: bytes, password: str) -> bytes:
-    from itertools import cycle
     return bytes(a ^ ord(b) for a, b in zip(file_data, cycle(password)))
 
 
 def decrypt_xor(encrypted_data: bytes, password: str) -> bytes:
-    from itertools import cycle
     return bytes(a ^ ord(b) for a, b in zip(encrypted_data, cycle(password)))
 
 
 def encrypt(file_data: bytes, password: str):
     key = generate_key(password)
     f = Fernet(key)
-    return f.encrypt(file_data)    
+    return f.encrypt(file_data)
 
 
 def decrypt(encrypted_data: bytes, password: str) -> bytes:
@@ -36,19 +36,22 @@ def derive_key(salt: str, password: str):
     kdf = Scrypt(salt=salt, length=32, n=2**4, r=16, p=1)
     return kdf.derive(password.encode())
 
+
 def generate_salt(size: int = 16):
     # return secrets.token_bytes(size)
     return b'\xdf\x06\xb24\x03G/Q\xfd\xa1\xf6\xf0\xc4\xf8f\xbb'
 
+
 def load_salt():
     return open("salt.salt", "rb").read()
+
 
 def generate_key(password: str):
     """
     Generates a key from a `password`.
     """
     salt = generate_salt()
-    
+
     # generate the key from the salt and the password
     derived_key = derive_key(salt, password)
 
